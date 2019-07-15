dfce48da88e4644181f23f896f5f95685c5c4e50
ragavanrocky <52520783+ragavanrocky@users.noreply.github.com>
2019-07-15 15:42:34 +0530

    Create ps16.py

---
+++ ps16.py
@@ -0,0 +1,12 @@
+
+cb=int(input())
+s=list(map(int,input().split()))
+a=0
+for f in range(cb):
+    for e in range(f,cb):  
+        for r in range(e,cb):
+            if s[f]<s[e]<s[r]:
+                a+=1
+print(a)
+
+fjghjgh
